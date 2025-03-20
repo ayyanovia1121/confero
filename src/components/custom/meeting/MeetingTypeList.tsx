fcb214d71c61339dc586fe54d982bbcf1ed94eb5
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-20 16:09:38 +0700

    feat(meeting): ✨ add meeting type list component

---
+++ src/components/custom/meeting/MeetingTypeList.tsx
@@ -0,0 +1,12 @@
+'use client'
+import MeetingCard from './MeetingCard';
+
+const MeetingTypeList = () => {
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
+      
+    </section>
+  );
+}
+
+export default MeetingTypeList
