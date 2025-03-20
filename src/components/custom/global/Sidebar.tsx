053e2805b0ad104ac0a940c6581590dcdac94798
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-20 12:38:28 +0700

    feat(sidebar): ✨ update sidebar with dynamic active state

--- src/components/custom/global/Sidebar.tsx
+++ src/components/custom/global/Sidebar.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
-    <div>Sidebar</div>
-  )
-}
+    <section className="sticky left-0 top-0 h-screen flex w-fit flex-col justify-between p-6 pt-28 bg-dark-1 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathname === link.route || pathname.startsWith(link.route);
 
-export default Sidebar
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={cn(
+                "flex items-center gap-4 rounded-lg p-4 justify-start",
+                isActive && "bg-blue text-primary-foreground"
+              )}
+            >
+              <Image
+                src={link.imgUrl}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-lg font-semibold max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
