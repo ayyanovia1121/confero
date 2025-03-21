1770a4ca1c8e1ef4e098ffa7c9a50312877452ed
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-21 13:48:10 +0700

    fix(ui): 🐛 ensure DialogTitle is present for better accessibility

--- src/components/custom/meeting/MeetingModal.tsx
+++ src/components/custom/meeting/MeetingModal.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import Image from "next/image";
 
 interface MeetingModalProps {
@@ -32,6 +33,9 @@ const MeetingModal = ({
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+        <DialogTitle>
+          <VisuallyHidden>Meeting Settings</VisuallyHidden>
+        </DialogTitle>
         <div className="flex flex-col gap-6">
           {image && (
             <div className="flex justify-center">
